09439a728874c86dd7f3fa5df7156b66b5f99067
Javier.Barreto <javier.barreto@ENCEDCREMLT0661.local>
2024-08-19 16:15:40 -0600

    Initialized backend and frontend

---
+++ Backend/flightchecker/src/main/java/com/breakabletoyii/flightchecker/FlightcheckerApplication.java
@@ -0,0 +1,13 @@
+package com.breakabletoyii.flightchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightcheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightcheckerApplication.class, args);
+	}
+
+}
---
+++ Backend/flightchecker/src/test/java/com/breakabletoyii/flightchecker/FlightcheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breakabletoyii.flightchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightcheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
