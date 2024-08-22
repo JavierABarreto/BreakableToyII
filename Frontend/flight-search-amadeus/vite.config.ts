d0b14e9a546a3cc5a7f4138922b0125977cc2a69
Javier.Barreto <javier.barreto@ENCEDCREMLT0661.local>
2024-08-22 16:07:41 -0600

    uploading local changes

--- Frontend/flight-search-amadeus/vite.config.ts
+++ Frontend/flight-search-amadeus/vite.config.ts
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 5173,
+    strictPort: true
+  },
   server: {
     host: true,
     strictPort: true,
-    port: 8080
+    port: 5173
   }
 })
