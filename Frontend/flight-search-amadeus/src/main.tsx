3968299dd450496e0a0b20018c292bf60e07a337
Javier.Barreto <javier.barreto@ENCEDCREMLT0661.local>
2024-08-27 11:23:49 -0600

    removing redux from the project

--- Frontend/flight-search-amadeus/src/main.tsx
+++ Frontend/flight-search-amadeus/src/main.tsx
@@ -2,15 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './scss/styles.scss'
 import 'bootstrap';
-import { Provider } from 'react-redux'
-import store from './js/redux/store.tsx'
 import { router } from './js/router.tsx'
 import { RouterProvider } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
