6e9ac1415934bcd3801553de613f6471ee2c10be
Javier.Barreto <javier.barreto@ENCEDCREMLT0661.local>
2024-08-27 17:43:02 -0600

    using mock data to display information on details view

--- Frontend/flight-search-amadeus/src/components/Details/Segments.tsx
+++ Frontend/flight-search-amadeus/src/components/Details/Segments.tsx
@@ -1,12 +1,26 @@
-export const Segments = () => {
+import dayjs from "dayjs"
+
+export const Segments = (data: any) => {
+  console.log(data)
+  const { aircraft, arrival, departure, id, number } = data?.data
+  const { at: arrivalAt, iataCode: arrivalIATA } = arrival
+  const { at: departureAt, iataCode: departureIATA } = departure
+
+  const arrivalDate = dayjs(arrivalAt).format("YYYY-MM-DD HH:mm")
+  const departureDate = dayjs(departureAt).format("YYYY-MM-DD HH:mm")
+
   return (
     <div className="container-sm border my-3 p-3">
       <div className="row">
         <div className="col-8">
-          <p>Segment title</p>
-          <p>YYYY-MM-DD HH:mm - YYYY-MM-DD HH:mm</p>
+          <div className="d-flex">
+            <h5 className="col-4">Segment {id}</h5>
+            <p className="col text-end">No. {number}</p>
+          </div>
+
+          <p>{departureDate} - {arrivalDate}</p>
           <p>{"CITY (CODE) - CITY (CODE)"}</p>
-          <p>AIRLINE XX</p>
+          <p>{`AIRLINE (XX) ${aircraft.code}`}</p>
         </div>
 
         <div className="col border" style={{ height: 170 }}>
---
+++ Frontend/flight-search-amadeus/src/js/price.tsx
@@ -0,0 +1,3 @@
+export const priceString = ( currency: Number, quantity: Number) => {
+  return `$${quantity} ${currency}`
+}
