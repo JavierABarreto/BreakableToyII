045705e6eaaf3349a24c44327e8f95415d5d9047
Javier.Barreto <javier.barreto@ENCEDCREMLT0661.local>
2024-09-02 13:42:01 -0600

    added a file that wasnt sent

--- Backend/flightchecker/src/main/java/com/javier/flightchecker/services/AccessTokenService.java
+++ Backend/flightchecker/src/main/java/com/javier/flightchecker/services/AccessTokenService.java
@@ -10,11 +10,14 @@ import java.net.http.HttpResponse;
 
 public class AccessTokenService {
     public String getAccessToken() throws Exception {
+        String clientId = "PlU5zrV8FblRIkZMF6B7tjZSo6rtk0qo"; // PlU5zrV8FblRIkZMF6B7tjZSo6rtk0qo
+        String clientSecret = "cUcm5KMeKKnNcw1C"; // cUcm5KMeKKnNcw1C
+
         try {
             String accessToken = "";
             URI uri = new URI("https://test.api.amadeus.com/v1/security/oauth2/token");
 
-            String requestBody = "grant_type=client_credentials&client_id=PlU5zrV8FblRIkZMF6B7tjZSo6rtk0qo&client_secret=cUcm5KMeKKnNcw1C";
+            String requestBody = "grant_type=client_credentials&client_id="+clientId+"&client_secret="+clientSecret;
 
             HttpRequest postRequest = HttpRequest.newBuilder()
                     .uri(uri)
